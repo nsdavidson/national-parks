180a2d617d584da3499ba552ea75343c69eefe94
Nolan Davidson <ndavidson@chef.io>
2017-04-04 12:09:13 -0400

    added package install outside of studio
    Signed-off-by: Nolan Davidson <ndavidson@chef.io>

--- .delivery/build_cookbook/recipes/publish.rb
+++ .delivery/build_cookbook/recipes/publish.rb
@@ -15,6 +15,12 @@ hab_build node['delivery']['change']['project'] do
   cwd node['delivery']['workspace']['repo']
   home_dir delivery_workspace
   action :build
+  notifies :run, 'execute[install-artifact]'
+end
+
+execute 'install-artifact' do
+  command "sudo hab pkg install #{plan_dir}/results/#{last_build_env['pkg_artifact']}"
+  action :nothing
   notifies :run, 'execute[export-container]'
 end
 
