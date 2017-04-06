8909fd8e1d4f90c71568171153c7dae19b05ccb2
Nolan Davidson <ndavidson@chef.io>
2017-04-06 09:49:18 -0400

    initial stab at getting the mongo ip and fleshing out the templates
    Signed-off-by: Nolan Davidson <ndavidson@chef.io>

--- .delivery/build_cookbook/recipes/default.rb
+++ .delivery/build_cookbook/recipes/default.rb
@@ -2,6 +2,8 @@ include_recipe 'habitat-build::default'
 
 docker_installation 'default'
 
+package 'jq'
+
 remote_file '/usr/local/bin/kubectl' do
   source 'https://storage.googleapis.com/kubernetes-release/release/v1.6.1/bin/linux/amd64/kubectl'
   owner 'root'
