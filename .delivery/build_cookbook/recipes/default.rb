58fa7fa99a460b284f66ae1dcd62810026588363
Nolan Davidson <ndavidson@chef.io>
2017-04-06 08:46:54 -0400

    add kubectl to default recipe
    Signed-off-by: Nolan Davidson <ndavidson@chef.io>

--- .delivery/build_cookbook/recipes/default.rb
+++ .delivery/build_cookbook/recipes/default.rb
@@ -1,3 +1,11 @@
 include_recipe 'habitat-build::default'
 
 docker_installation 'default'
+
+apt_repository 'kubernees' do
+  uri 'http://apt.kubernetes.io/'
+  components 'main'
+  key 'https://packages.cloud.google.com/apt/doc/apt-key.gpg'
+end
+
+package 'kubectl'
