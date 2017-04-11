733a534d2bdbb5d55f6356b780ae4083580e0273
Nolan Davidson <ndavidson@chef.io>
2017-04-11 09:59:21 -0400

    moving retry logic for smoke tests into the actual test
    Signed-off-by: Nolan Davidson <ndavidson@chef.io>

--- .delivery/build_cookbook/test/smoke/default/smoke.rb
+++ .delivery/build_cookbook/test/smoke/default/smoke.rb
@@ -1,7 +1,24 @@
 my_host_endpoint = attribute('host_endpoint', default: 'http://www.google.com', description: 'The host endpoint to test')
 
-describe http(my_host_endpoint) do
-  its('status') { should cmp 200 }
-  its('body') { should match /National Parks/ }
-  its('headers.content-type') { should match /text\/html/ }
+begin
+  require 'resolv'
+  require 'uri'
+  retries ||= 0
+  Resolv.getaddress(URI.parse(my_host_endpoint).host)
+  describe http(my_host_endpoint) do
+    its('status') { should cmp 200 }
+    its('body') { should match /National Parks/ }
+    its('headers.content-type') { should match /text\/html/ }
+  end
+rescue
+  puts "Waiting for #{my_host_endpoint} to become available..."
+  sleep 10
+  if (retries += 1) < 15
+    retry
+  else
+    raise "Failed to resolve #{my_host_endpoint} after 150 seconds..."
+  end
 end
+
+  
+
